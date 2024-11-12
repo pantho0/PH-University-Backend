07517f8252a69133471252c9d6c1d47e3eecb2b1
pantho0 <panthomashreky6@gmail.com>
2024-11-12 22:52:31 +0600

    added academic modules necessary files

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -14,11 +14,29 @@ const findLastStudentId = async () => {
     })
     .lean();
 
-  return lastStudent?.id ? lastStudent?.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent?.id : undefined;
 };
 
 export const generateStudentId = async (payload: TAcademicSemester) => {
-  const currentId = (await findLastStudentId()) || (0).toString();
+  let currentId = (0).toString(); //default 0000
+  // we got there full id (ex:2030010001)
+  const lastStudentId = await findLastStudentId();
+  // we got 01 as semester code
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6);
+  // we got 2030 as year code
+  const lastStudentYear = lastStudentId?.substring(0, 4);
+  //we got current year from the payload
+  const currentYear = payload.year;
+  //we got current semester code from the payload
+  const currentSemesterCode = payload.code;
+  //compare the current year with the last student year and current semester code with the last student semester code
+  if (
+    lastStudentId &&
+    lastStudentYear === currentYear &&
+    lastStudentSemesterCode === currentSemesterCode
+  ) {
+    currentId = lastStudentId.substring(6);
+  }
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
   incrementId = `${payload.year}${payload.code}${incrementId}`;
   return incrementId;
